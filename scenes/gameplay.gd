7aaf6a1df71b581602932b3c517b331afb62a6bc
CallmeAlien <cfc953@gmail.com>
2026-05-21 00:59:13 +0100

    Concept image for sizing and layout
    Refined player movement in both directions and leaning on move

--- scenes/gameplay.gd
+++ scenes/gameplay.gd
@@ -3,6 +3,8 @@ extends Node2D
 var ball_scene: PackedScene = load("res://scenes/ball.tscn")
 @export var ball_in_play: Ball = null
 
+signal ball_killed
+
 func _ready():
 	spawn_new_ball()
 
@@ -10,9 +12,18 @@ func spawn_new_ball():
 	ball_in_play = ball_scene.instantiate()
 	$Balls.add_child(ball_in_play)
 	ball_in_play.position = Vector2(get_viewport_rect().size.x * 0.5, 0)
-	pass
+	_freeze_ball_in_play()
 
 func _on_kill_floor_killed_ball() -> void:
-	print("Killed ball! Spawn new ball!!")
 	ball_in_play.free()
 	call_deferred("spawn_new_ball")
+	ball_killed.emit()
+
+func _on_player_full_lean() -> void:
+	_release_frozen_ball()
+
+func _freeze_ball_in_play():
+	ball_in_play.freeze = true
+
+func _release_frozen_ball():
+	ball_in_play.freeze = false
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
+enum Lean_Direction {
+LEFT = -1,
+RIGHT = 1
+}
+
 @export var speed = 500
 
-@export var movement_lean_factor = 0.0
+@export_range(0.0, 15.0, 0.1) var movement_lean_angle = 0.0
 
 @export var poke_force: float = 15
 @export var max_fling_force : float = -1500
@@ -23,25 +28,23 @@ var charge_held_time :float = 0.0
 
 @export var ball_fling_multiplier : float = 2.0
 
+signal ready_to_serve
+var has_served: bool = false
+@export_range(0.0, 1.0, 0.1) var serve_ready_factor: float = 0.8
+
 func _ready() -> void:
-	var children = get_all_children($BodyBase)
+	var children = _get_all_children($BodyBase)
 	head = children[children.size() - 1]
 
 func _process(delta: float):
-	if Input.is_action_just_pressed("move_up"):
-		charge_held_time = calculate_charge_held_start_time()
-		calculate_charge_held_lean(0.0)
-	elif Input.is_action_pressed("move_up"):
-		calculate_charge_held_lean(delta)
-	elif Input.is_action_just_released("move_up"):
-		apply_fling_force_on_charge_released()
-	
-	update_visuals(delta)
+	_process_charge_time(delta)
+	_update_visuals(delta)
 
 func _physics_process(_delta: float):
 	move_and_slide()
-	spring.AddVelocity(-velocity.x * _delta * movement_lean_factor)
-	simulate_spring_motion(_delta)
+	if _charge_held_time_normalised() <= 0.0:
+		spring.cEquilibrium = ((abs(velocity.x) / speed) * movement_lean_angle * -velocity.normalized().x)
+	_simulate_spring_motion(_delta)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch and event.is_pressed():
@@ -63,53 +66,72 @@ func _input(event: InputEvent) -> void:
 	else:
 		velocity = Vector2.ZERO
 
-func update_visuals(_delta):
-	var children = get_all_children($BodyBase)
+func _process_charge_time(delta:float):
+	if Input.is_action_just_pressed("move_up"):
+		charge_held_time = _calculate_charge_held_start_time()
+		_calculate_charge_held_lean(0.0, Lean_Direction.RIGHT)
+	elif Input.is_action_pressed("move_up"):
+		_calculate_charge_held_lean(delta, Lean_Direction.RIGHT)
+		_signal_ready_to_serve()
+	elif Input.is_action_just_released("move_up"):
+		apply_fling_force_on_charge_released(Lean_Direction.RIGHT)
+
+	if Input.is_action_just_pressed("move_down"):
+		charge_held_time = _calculate_charge_held_start_time()
+		_calculate_charge_held_lean(0.0, Lean_Direction.LEFT)
+	elif Input.is_action_pressed("move_down"):
+		_calculate_charge_held_lean(delta, Lean_Direction.LEFT)
+		_signal_ready_to_serve()
+	elif Input.is_action_just_released("move_down"):
+		apply_fling_force_on_charge_released(Lean_Direction.LEFT)
+
+func _update_visuals(_delta):
+	var children = _get_all_children($BodyBase)
 	var rotFactor = spring.GetPosition() / children.size();
 	for c in children:
 		c.rotation = deg_to_rad(rotFactor)
 
-func simulate_spring_motion(delta: float):
+func _simulate_spring_motion(delta: float):
 	if use_fast_spring_motion:
 		SpringMotion.calc_damped_simple_harmonic_motion_fast(spring, delta)
 	else:
 		SpringMotion.calc_damped_simple_harmonic_motion(spring, delta)
 
-func get_all_children(node:Node2D) -> Array:
+func _get_all_children(node:Node2D) -> Array:
 	var all_children := []
 	for child in node.get_children():
 		if child is Sprite2D:
 			all_children.append(child)
-		for grand_child in get_all_children(child):
+		for grand_child in _get_all_children(child):
 			all_children.append(grand_child)
 
 	return all_children
 
 func _on_head_entered(body: Node2D) -> void:
 	# FIGURE OUT HOW TO CANCEL A HELD INPUT FROM HERE SO THE FLING ALWAYS APPLIES IMMEDIATELY
-	apply_bullet_header_time()
+	#_apply_bullet_header_time()
 	if body is Ball:
 		var direction : Vector2 = body.global_position - head.global_position
 		body.linear_velocity = direction.normalized() * abs(spring.cVelocity) * ball_fling_multiplier
 
-func charge_held_time_normalised() -> float:
+func _charge_held_time_normalised() -> float:
 	return charge_held_time / max_charge_hold_time
 
-func calculate_charge_held_start_time() -> float:
+func _calculate_charge_held_start_time() -> float:
 	var total_max_lean : float = min_lean_applied_on_charge + max_lean_applied_on_charge
 	return (min_lean_applied_on_charge/total_max_lean) * max_charge_hold_time
 
-func calculate_charge_held_lean(delta:float):
+func _calculate_charge_held_lean(delta:float, direction:Lean_Direction):
 	charge_held_time = clamp(charge_held_time + delta, 0.0, max_charge_hold_time)
-	spring.cEquilibrium = min_lean_applied_on_charge + (charge_held_time_normalised() * max_lean_applied_on_charge) 
+	spring.cEquilibrium = (min_lean_applied_on_charge + (_charge_held_time_normalised() * max_lean_applied_on_charge)) * direction
 
-func apply_fling_force_on_charge_released():
+func apply_fling_force_on_charge_released(direction:Lean_Direction):
 	spring.cEquilibrium = 0.0
-	spring.AddVelocity(max_fling_force * charge_held_time_normalised())
+	spring.AddVelocity(-max_fling_force * _charge_held_time_normalised() * direction)
 	charge_held_time = 0.0
 
-func apply_bullet_header_time():
-	var bullet_time_modifier = clamp(spring.cVelocity / max_fling_force, 0, 1)
+func _apply_bullet_header_time():
+	var bullet_time_modifier = clamp(abs(spring.cVelocity / max_fling_force), 0, 1)
 	var bullet_time_adjusted = bullet_header_time * bullet_time_modifier
 	if (bullet_time_adjusted >0.2):
 		$BulletHeaderTimer.start(bullet_time_adjusted)
@@ -117,3 +139,11 @@ func apply_bullet_header_time():
 
 func _on_bullet_header_timeout() -> void:
 	Engine.time_scale = 1.0
+
+func _on_gameplay_ball_killed() -> void:
+	has_served = false
+
+func _signal_ready_to_serve():
+	if _charge_held_time_normalised() >= 0.8 && !has_served:
+		ready_to_serve.emit()
+		has_served = true
